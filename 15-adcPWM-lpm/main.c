bd1e451882c8720bc42177622406adca1a6d0c7c
chrisL <chrisrlawrence2005@outlook.com>
2025-09-19 21:04:30 +1200

    Re-enabled LPM

--- 15-adcPWM-lpm/main.c
+++ 15-adcPWM-lpm/main.c
@@ -15,30 +15,31 @@ void main(void) {
     P1SEL0 |= AIN;
     P1SEL1 |= AIN;
 
-	ADCMCTL0 |= ADCINCH_1;                      // VCC as reference, A1 as ADC input
+	ADCMCTL0 |= ADCINCH_1;                       // VCC as reference, A1 as ADC input
 	ADCCTL1 |= ADCSHP;
 	ADCIE |= ADCIE0;                             // Enable interrupt on ADC conversion complete
-	ADCCTL0 |= ADCSHT_4 + ADCON;                // Sample and hold for 64 clock cycles, enable ADC
+	ADCCTL0 |= ADCSHT_4 + ADCON;                 // Sample and hold for 64 clock cycles, enable ADC
 
-    TB0CCR0 = 1023;								// Set Timer0 PWM Period
-    TB0CCTL0 = CCIE;							// Enable Overflow Interrupt
-	TB0CCTL1 = OUTMOD_7;						// Set TB0.1 Waveform Mode
-	TB0CCR1 = 0;								// Set TB0.1 PWM toggle point
-	TB0CTL = TBSSEL_2 + MC_1;					// Timer Clock -> SMCLK, Mode -> Count up
+    TB0CCR0 = 1023;								 // Set Timer0 PWM Period
+    TB0CCTL0 = CCIE;							 // Enable Overflow Interrupt
+	TB0CCTL1 = OUTMOD_7;						 // Set TB0.1 Waveform Mode
+	TB0CCR1 = 0;								 // Set TB0.1 PWM toggle point
+	TB0CTL = TBSSEL_2 + MC_1;					 // Timer Clock -> SMCLK, Mode -> Count up
 
-	PM5CTL0 &= ~LOCKLPM5;                       // Unlock GPIO
+	PM5CTL0 &= ~LOCKLPM5;                        // Unlock GPIO
 
-	__bis_SR_register(GIE);
-   	while(1); //__bis_SR_register(LPM0_bits + GIE);			// Go into LPM0, enable CPU interrupts
+   	while(1){
+		__bis_SR_register(LPM0_bits + GIE); // Go into LPM0, enable CPU interrupts
+	}
 }
 
-#pragma vector=ADC_VECTOR						// ADC Conversion Complete Interrupt Vector
+#pragma vector=ADC_VECTOR						 // ADC Conversion Complete Interrupt Vector
 __interrupt void ADC_ISR (void) {
-	TB0CCR1 = ADCMEM0;							// Set duty cycle = ADC value
-	ADCIFG &= ~ADCIFG0;                         // Clear interrupt flag
+	TB0CCR1 = ADCMEM0;							 // Set duty cycle = ADC value
+	ADCIFG &= ~ADCIFG0;                          // Clear interrupt flag
 }
 
-#pragma vector = TIMER0_B0_VECTOR				// TB0CCR0 interrupt vector
+#pragma vector = TIMER0_B0_VECTOR				 // TB0CCR0 interrupt vector
 __interrupt void CCR0_ISR(void) {
     // If the ADC is idle start a new conversion
 	if(!(ADCCTL1 & ADCBUSY)) {
